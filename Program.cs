33d8ff1cfc2e5a37341138125aa7a06d70f019bc
white <white@example.com>
2025-10-21 21:34:49 +0500

    Add greetWhite

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ namespace pepe
 {
     class sys
     {
+        public static void greetWhite()
+        {
+            Console.WriteLine("hello white");
+        }
         public static void Main()
         {
             Console.WriteLine("Hello");
